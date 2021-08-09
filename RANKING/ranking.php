d6a1fbdc418654be83a7e6223daed46bf1ab5b2e
Amaliamelo <amalia.melo@aluno.ifsp.edu.br>
2021-08-09 15:44:57 -0300

    errrossss

--- RANKING/ranking.php
+++ RANKING/ranking.php
@@ -4,33 +4,32 @@ include "../conexao.php";
 include "../ABC/cabecalho_abc.php";
 include "../ABC/menu_abc.php";
 
-    $consulta = "SELECT usuario.nome as nome, cod_subfase, sum(usuario_subfase.qtd_acertos) AS acertos_total,
+    $consulta = "SELECT usuario.email as email, cod_subfase, sum(usuario_subfase.qtd_acertos) AS acertos_total,
     subfase.cod_fase as fase, subfase.nome as nome_subfase FROM usuario_subfase INNER JOIN usuario
     on usuario_subfase.cod_usuario=usuario.id_usuario INNER JOIN subfase on 
-    subfase.id_subfase=usuario_subfase.cod_subfase GROUP BY usuario.nome,cod_subfase 
+    subfase.id_subfase=usuario_subfase.cod_subfase GROUP BY usuario.nome,cod_fase 
     ORDER BY acertos_total DESC";
-    print_r($consulta);
     $resultado = mysqli_query($conexao,$consulta) or die("Erro na consulta1");
 ?>
-<main class="bodyscores" style="height: 100%; padding-top:3%; padding-bottom:17%;" >
-<div class="container  mt-4">
-    <table class="table border">
+<main class="bodyscores" style="padding-top:3%; padding-bottom:15.7%" >
+<div class="container align-middle pt-5 mt-5 mp-5">
+    <table class="table border pt-5">
         <thead class="table-success">
             <tr>
-                <th scope="col">Posição</th>
-                <th scope="col">Usuário</th>
-                <th scope="col">Acertos</th>
-                <th scope="col">Subfase</th>
-                <th scope="col">Fase</th>
+                <th scope="col-sm-2">Posição</th>
+                <th scope="col-sm-2">Usuário</th>
+                <th scope="col-sm-2">Acertos</th>
+                <th scope="col-sm-2">Subfase</th>
+                <th scope="col-sm-2">Fase</th>
             </tr>
         </thead>
         <?php 
         $posicao_usuario=0;
         while($linha=mysqli_fetch_assoc($resultado)){
             $posicao_usuario++;
-            echo'<tr class="table-warning">
-                <th scope="row" >'.$posicao_usuario.'</th>
-                <td >'.$linha["nome"].'</td>
+            echo'<tr class="table-dark flex-column justify-content-center align-items-center">
+                <th scope="row-sm-2" >'.$posicao_usuario.'</th>
+                <td >'.$linha["email"].'</td>
                 <td>'.$linha["acertos_total"].'</td>
                 <td>'.$linha["nome_subfase"].'</td>
                 <td>'.$linha["fase"].'</td>
